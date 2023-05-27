ab8078424ea859aef0b0c450523d52314e461cb8
juhui88 <sand8594@hufs.ac.kr>
2023-05-27 23:47:03 +0900

    first commit

---
+++ NomadWeather/App.js
@@ -0,0 +1,26 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>된당</Text>
+      <Text>히하</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text:{
+    fontSize:48,
+    color:'red'
+
+  }
+});
